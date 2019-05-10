ad148af998374c25838c875e46993003e7a61022
Nikko Miu <nikkoamiu@gmail.com>
2019-05-10 11:46:30 -0500

    Update conn.go

--- conn.go
+++ conn.go
@@ -1,19 +1,21 @@
 package hub
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 )
 
+// Conn is the wrapped connection for a client
 type Conn struct {
 	WSConn *websocket.Conn
 	Topic  string
 	Room   string
 }
 
+// SendJSON sends a message to the client connection
 func (c Conn) SendJSON(event string, payload interface{}) {
-	c.WSConn.WriteJSON(CoreMessage{
+	c.WSConn.WriteJSON(coreMessage{
 		Topic:   c.Topic,
 		Room:    c.Room,
 		Event:   event,
@@ -21,6 +23,7 @@ func (c Conn) SendJSON(event string, payload interface{}) {
 	})
 }
 
+// GetChannel gets the fully named channel in the form of "<topic>:<room>"
 func (c Conn) GetChannel() string {
 	return fmt.Sprintf("%s:%s", c.Topic, c.Room)
 }
