7fb300b0352c420591044ce0a675991fae9ffe97
Nikko Miu <nikkoamiu@gmail.com>
2019-05-10 11:46:42 -0500

    Update Hub for Inline DOcs (#16)

--- hub.go
+++ hub.go
@@ -27,44 +27,46 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-type CoreMessage struct {
+type coreMessage struct {
 	Topic   string      `json:"topic"`
 	Room    string      `json:"room"`
 	Event   string      `json:"event"`
 	Payload interface{} `json:"payload"`
 }
 
-type StatusPayload struct {
+type statusPayload struct {
 	Status string `json:"description,omitempty"`
 	Error  string `json:"error,omitempty"`
 }
 
+// TopicHandler is the interface that must be implemented to register
+// a new topic handler with the Hub for processing messages
 type TopicHandler interface {
 	Join(conn Conn, msg interface{}) error
 	Receive(conn Conn, event string, msg interface{})
 	Unjoin(conn Conn)
 }
 
+// Hub is the struct for the main Hub
 type Hub struct {
-	broadcast chan CoreMessage
-
 	topicHandlers map[string]TopicHandler
 
 	clients map[*websocket.Conn][]string
 }
 
+// New creates a new hub and starts the ping service to keep connections alive
 func New() (h *Hub) {
 	h = &Hub{
 		clients:       make(map[*websocket.Conn][]string),
 		topicHandlers: make(map[string]TopicHandler),
 	}
 
-	go h.startBroadcaster()
 	go h.runPinger()
 
 	return
 }
 
+// AddTopicHandler registers a new TopicHandler with the topic name
 func (h *Hub) AddTopicHandler(topic string, handler TopicHandler) {
 	h.topicHandlers[topic] = handler
 }
@@ -78,20 +80,7 @@ func (h *Hub) runPinger() {
 	}
 }
 
-// TODO determine if this is still needed at the hub level
-func (h Hub) startBroadcaster() {
-	for {
-		msg := <-h.broadcast
-		for conn := range h.clients {
-			err := conn.WriteJSON(msg)
-			if err != nil {
-				log.Println("[ERR] could not write message:", err)
-				h.disconnect(conn)
-			}
-		}
-	}
-}
-
+// Handler is the HTTP handler for WebSocket connections to go to
 func (h *Hub) Handler(w http.ResponseWriter, r *http.Request) {
 	// Upgrade the connection to a websocket
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -114,7 +103,7 @@ func (h *Hub) Handler(w http.ResponseWriter, r *http.Request) {
 	// Enter event loop
 	for {
 		// Read the message
-		var msg CoreMessage
+		var msg coreMessage
 		err := conn.ReadJSON(&msg)
 		if err != nil {
 			// Log errors if not expected message
@@ -136,29 +125,29 @@ func (h *Hub) Handler(w http.ResponseWriter, r *http.Request) {
 		// Check that the topic for the message exists
 		topic := h.topicHandlers[msg.Topic]
 		if topic == nil {
-			channelConn.SendJSON("not_found", StatusPayload{Status: "error", Error: "topic not found"})
+			channelConn.SendJSON("not_found", statusPayload{Status: "error", Error: "topic not found"})
 			continue
 		}
 
 		if msg.Event == "join" {
 			// If the user is already joined dont rejoin
 			if h.isClientInTopic(channelConn, channelConn.GetChannel()) {
-				channelConn.SendJSON("already_joined", StatusPayload{Status: "error", Error: "already joined topic"})
+				channelConn.SendJSON("already_joined", statusPayload{Status: "error", Error: "already joined topic"})
 				continue
 			}
 
 			if err := topic.Join(channelConn, msg.Payload); err != nil {
-				channelConn.SendJSON("join_failed", StatusPayload{Status: "error", Error: err.Error()})
+				channelConn.SendJSON("join_failed", statusPayload{Status: "error", Error: err.Error()})
 				continue
 			}
 
 			h.clients[conn] = append(h.clients[conn], channelConn.GetChannel())
 
-			channelConn.SendJSON("joined", StatusPayload{Status: "success"})
+			channelConn.SendJSON("joined", statusPayload{Status: "success"})
 		} else if h.isClientInTopic(channelConn, channelConn.GetChannel()) {
 			topic.Receive(channelConn, msg.Event, msg.Payload)
 		} else {
-			channelConn.SendJSON("not_joined", StatusPayload{Status: "error", Error: "topic not joined"})
+			channelConn.SendJSON("not_joined", statusPayload{Status: "error", Error: "topic not joined"})
 		}
 	}
 }
