3cd5eab08096f64aae7295e52ae59ea36c5fffd2
Nikko Miu <nikkoamiu@gmail.com>
2019-05-10 10:24:53 -0500

    Update socket.js

--- js/socket.js
+++ js/socket.js
@@ -1,18 +1,19 @@
 let ws;
 let timeout = 1000;
 
-class Channel {
+class Room {
   ws = null;
   isJoined = false;
   topic = '';
-  channel = '';
+  room = '';
+  eventHandlers = {};
 
-  constructor(ws, fullTopic) {
+  constructor(ws, fullName) {
     this.ws = ws;
 
-    const [topic, chan] = fullTopic.split(':');
+    const [topic, room] = fullName.split(':');
     this.topic = topic;
-    this.channel = chan;
+    this.room = room;
   }
 
   join() {
@@ -22,8 +23,23 @@ class Channel {
 
     this.ws.send(JSON.stringify({
       topic: this.topic,
-      event: 'join_topic',
+      room: this.room,
+      event: 'join',
     }));
+  }
+
+  onEvent(event, handler) {
+    if (!this.eventHandlers[event]) {
+      this.eventHandlers[event] = [];
+    }
+
+    this.eventHandlers[event].push(handler);
+  }
+
+  messageReceived(event, payload) {
+    if (this.eventHandlers[event]) {
+      this.eventHandlers[event].forEach(h => h(event, payload));
+    }
   }
 }
 
@@ -32,12 +48,12 @@ class Socket {
   url = '';
   opts = {
     timeout: 1000,
-    onOpen: e => null,
-    onError: e => null,
-    onClose: e => null,
   };
   currentTimeout = 0;
-  channels = {};
+  rooms = {};
+  openHandlers = [];
+  closeHandlers = [];
+  errorHandlers = [];
 
   constructor(url, opts) {
     this.url = url;
@@ -47,60 +63,66 @@ class Socket {
     this.connect = this.connect.bind(this);
     this.onOpen = this.onOpen.bind(this);
     this.onClose = this.onClose.bind(this);
-    this.onMessage = this.onMessage.bind(this);
     this.onError = this.onError.bind(this);
-
-    this.connect();
   }
 
   connect() {
     this.ws = new WebSocket(this.url);
-    this.ws.addEventListener('open', this.onOpen);
-    this.ws.addEventListener('close', this.onClose);
-    this.ws.addEventListener('message', this.onMessage);
-    this.ws.addEventListener('error', this.onError);
+    this.ws.addEventListener('open', e => {
+      console.info('connected to socket at:', this.url);
+
+      this.currentTimeout = this.opts.timeout;
+
+      // Call all the user defined callbacks
+      this.openHandlers.forEach(fn => fn(e));
+    });
+
+    this.ws.addEventListener('close', e => {
+      if (!e.wasClean) {
+        console.error('Socket closed:', e);
+        setTimeout(this.connect, this.currentTimeout);
+        this.currentTimeout = this.currentTimeout * 1.5;
+      }
+
+      // Call all the user defined callbacks
+      this.closeHandlers.forEach(fn => fn(e));
+    });
+
+    this.ws.addEventListener('message', e => {
+      const msg = JSON.parse(e.data);
+
+      const channel = msg.topic+":"+msg.room;
+      if (this.rooms[channel]) {
+        this.rooms[channel].messageReceived(msg.event, msg.payload);
+      } else {
+        console.warn(`Channel (${channel}) not found for message:`, msg);
+      }
+    });
+
+    this.ws.addEventListener('error', e => {
+      console.error('Socket error:', e);
+
+      // Call all the user defined callbacks
+      this.errorHandlers.forEach(fn => fn(e));
+    });
   }
 
-  join(channelName) {
-    const channel = new Channel(this.ws, channelName);
-    this.channels[channelName] = channel;
-    channel.join();
-    return channel;
+  join(roomName) {
+    const room = new Room(this.ws, roomName);
+    this.rooms[roomName] = room;
+    room.join();
+    return room;
   }
 
-  onOpen(e) {
-    console.info('connected to socket at:', this.url);
-
-    this.currentTimeout = this.opts.timeout;
-
-    if (this.opts.onOpen) {
-      this.opts.onOpen(e);
-    }
+  onOpen(fn) {
+    this.openHandlers.push(fn);
   }
 
-  onClose(e) {
-    if (!e.wasClean) {
-      console.error('Socket closed:', e);
-      setTimeout(this.connect, this.currentTimeout);
-      this.currentTimeout = this.currentTimeout * 1.5;
-    }
-
-    if (this.opts.onClose) {
-      this.opts.onClose(e);
-    }
+  onClose(fn) {
+    this.closeHandlers.push(fn);
   }
 
-  onError(e) {
-    console.error('Socket error:', e)
-
-    if (this.opts.onError) {
-      this.opts.onError(e);
-    }
-  }
-
-  onMessage(e) {
-    const msg = JSON.parse(e.data);
-    console.log(msg);
-    this.channels[msg.topic]
+  onError(fn) {
+    this.errorHandlers.push(fn);
   }
 }
