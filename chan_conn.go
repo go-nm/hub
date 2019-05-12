6c412715879b855cb5009089355ca539098a56e9
Nikko Miu <nikko.miu@tyvoid.com>
2019-05-12 07:50:09 -0500

    Add concurrent write and connection cb fn

---
+++ chan_conn.go
@@ -0,0 +1,23 @@
+package hub
+
+import "fmt"
+
+// ChanConn is the connection given to the topic handlers
+// it contains the connection information and the context of
+// the current channel
+type ChanConn struct {
+	Conn *Conn
+
+	Topic string
+	Room  string
+}
+
+// SendJSON sends a message to the client connection
+func (c ChanConn) SendJSON(event string, payload interface{}) {
+	c.Conn.SendJSON(c.Topic, c.Room, event, payload)
+}
+
+// GetChannel gets the fully named channel in the form of "<topic>:<room>"
+func (c ChanConn) GetChannel() string {
+	return fmt.Sprintf("%s:%s", c.Topic, c.Room)
+}
--- conn.go
+++ conn.go
@@ -1,29 +1,27 @@
 package hub
 
 import (
-	"fmt"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 // Conn is the wrapped connection for a client
 type Conn struct {
-	WSConn *websocket.Conn
-	Topic  string
-	Room   string
+	WS       *websocket.Conn
+	Channels []string
+	ConnData []interface{}
+	mux      sync.Mutex
 }
 
 // SendJSON sends a message to the client connection
-func (c Conn) SendJSON(event string, payload interface{}) {
-	c.WSConn.WriteJSON(coreMessage{
-		Topic:   c.Topic,
-		Room:    c.Room,
+func (c *Conn) SendJSON(topic, room, event string, payload interface{}) {
+	c.mux.Lock()
+	c.WS.WriteJSON(coreMessage{
+		Topic:   topic,
+		Room:    room,
 		Event:   event,
 		Payload: payload,
 	})
-}
-
-// GetChannel gets the fully named channel in the form of "<topic>:<room>"
-func (c Conn) GetChannel() string {
-	return fmt.Sprintf("%s:%s", c.Topic, c.Room)
+	c.mux.Unlock()
 }
